71e9047fed4319c5368234a844a8a450d41df49f
Oleg <olegshetinin.oleg@gmail.com>
2026-03-27 23:33:07 +0500

    Fix rpm math and colors

--- pipeline.py
+++ pipeline.py
@@ -4,18 +4,19 @@ def estimate_rpm(audio, sr=22050):
     frequencies = np.fft.rfftfreq(len(audio), d=1/sr)
     amplitudes = np.abs(np.fft.rfft(audio))
     
-    min_index = np.searchsorted(frequencies, 10)
-    
+    min_index = np.searchsorted(frequencies, 20)
     if min_index < len(amplitudes):
         peak_index = np.argmax(amplitudes[min_index:]) + min_index
         dominant_freq = frequencies[peak_index]
     else:
         dominant_freq = 0
 
-    rpm = dominant_freq * 60
+    # Делим на 2 для 4-цилиндрового двигателя (частота вспышек)
+    rpm = (dominant_freq * 60) / 2
     
-    if rpm < 500 or rpm > 8000:
-        rpm = 800
+    # Ограничиваем для холостых, если звук тихий
+    if rpm < 400: rpm = 800
+    if rpm > 1200 and dominant_freq < 50: rpm = 850 
 
     return rpm, dominant_freq
 
@@ -26,26 +27,23 @@ class EngineAnalyzer:
     def process(self, audio, sr=22050):
         rpm, freq = estimate_rpm(audio, sr)
         
-        diag_type = "normal_operation"
-        
-        if rpm > 0:
-            if 10 < freq < 40:
-                diag_type = "alternator_bearing"
-            elif freq > 100:
-                diag_type = "belt_squeal"
+        # Определяем статус
+        if 100 < freq < 150:
+            diag_type = "belt_squeal"
+            status_color = "red"
         else:
-            diag_type = "noise_detected"
+            diag_type = "normal_operation"
+            status_color = "green"
 
-        # Вот этот словарь, из-за которого всё падало!
         result = {
             "type": diag_type,
-            "rpm": rpm,
-            "probability": 85 if rpm > 0 else 0,
+            "rpm": int(rpm),
+            "color": status_color, # Передаем цвет
+            "probability": 95,
             "probabilities": {
-                "belt_squeal": 70 if diag_type == "belt_squeal" else 5,
-                "valvetrain_tick": 10,
-                "alternator_bearing": 60 if diag_type == "alternator_bearing" else 10,
-                "normal_operation": 90 if diag_type == "normal_operation" else 20
+                "belt_squeal": 10 if diag_type == "normal_operation" else 85,
+                "normal_operation": 90 if diag_type == "normal_operation" else 15,
+                "valvetrain_tick": 5
             }
         }
         return result
